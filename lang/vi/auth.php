000ff15b4a328a47a845a9538294992534deb699
lokey2411 <hahaiviet24112003@gmail.com>
2024-04-08 08:23:08 +0700

    init prj

---
+++ lang/vi/auth.php
@@ -0,0 +1,87 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Những thông tin xác thực này không khớp với hồ sơ của chúng tôi.',
+    'throttle' => 'Quá nhiều lần đăng nhập. Vui lòng thử lại sau :seconds seconds.',
+    'account_deactivate' => 'Tài khoản của bạn hiện không hoạt động, vui lòng liên hệ với quản trị viên.',
+    'email_verify' => 'Vui lòng xác minh email của bạn trước khi đăng nhập.',
+
+    'full_name' => 'Tên đầy đủ',
+    'email' => 'Email',
+    'password' => 'Mật khẩu',
+    'confirm_password' => 'Xác nhận mật khẩu',
+    'remember_me' => 'Nhớ tôi',
+    'sign_in' => 'Đăng nhập',
+    'get_started' => 'Bắt đầu',
+    'sign_up' => 'Đăng ký',
+    'sign_out' => 'Đăng xuất',
+    'register' => 'Đăng ký',
+    'modern_&_powerful_interface' => 'Giao diện hiện đại và mạnh mẽ',
+    "choose_a_plan_that's_right_for_you" => "Hãy chọn gói phù hợp với bạn",
+    'stories_from_our_customers' => 'Câu chuyện từ khách hàng của chúng tôi',
+    "let's_hear_what_they_say" => "Hãy nghe những gì họ nói",
+    'subscribe_here' => 'Đăng ký tại đây',
+    'copyright_by' => 'Bản quyền của',
+    'home' => 'Trang chủ',
+    'about' => 'Giới thiệu',
+    'pricing' => 'Giá',
+    'contact' => 'Liên hệ',
+    'features' => 'Tính năng',
+
+
+    'login' => [
+        'title' => 'Đăng nhập để bắt đầu phiên của bạn',
+        'forgot_password' => 'Tôi quên mật khẩu',
+        'register_membership' => 'Đăng ký thành viên mới',
+    ],
+
+    'registration' => [
+        'title' => 'Đăng ký thành viên mới',
+        'i_agree' => 'Tôi đồng ý',
+        'terms' => 'điều khoản',
+        'have_membership' => 'Tôi đã có tư cách thành viên',
+    ],
+
+    'forgot_password' => [
+        'title' => 'Nhập Email để đặt lại mật khẩu',
+        'send_pwd_reset' => 'Gửi liên kết đặt lại mật khẩu',
+    ],
+
+    'reset_password' => [
+        'title' => 'Đặt lại mật khẩu của bạn',
+        'reset_pwd_btn' => 'Đặt lại mật khẩu',
+    ],
+
+    'emails' => [
+        'password' => [
+            'reset_link' => 'Nhấp vào đây để đặt lại mật khẩu của bạn',
+        ],
+    ],
+
+    'app' => [
+        'member_since' => 'Thành viên kể từ',
+        'messages' => 'Tin nhắn',
+        'settings' => 'Cài đặt',
+        'lock_account' => 'Khoá tài khoản',
+        'profile' => 'Hồ sơ',
+        'online' => 'Trực tuyến',
+        'search' => 'Tìm kiếm',
+        'create' => 'Tạo',
+        'export' => 'Xuất',
+        'print' => 'In',
+        'reset' => 'Reset',
+        'reload' => 'Tải lại',
+    ],
+];
---
+++ lang/vi/crud.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'add_new' => 'Thêm mới',
+    'cancel' => 'Huỷ',
+    'save' => 'Lưu',
+    'edit' => 'Sửa',
+    'detail' => 'Chi tiết',
+    'back' => 'Quay lại',
+    'action' => 'Hoạt động',
+    'id' => 'Id',
+    'created_at' => 'Được tạo tại',
+    'updated_at' => 'Cập nhật tại',
+    'deleted_at' => 'Đã xóa tại',
+    'are_you_sure' => 'Bạn có chắc không?',
+];
